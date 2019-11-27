91ac97808487091bd5653d8a88f751ce1ef7d284
tutkuince <tutku.ince@outlook.com>
2019-11-27 13:05:57 +0300

    info about annotations and work flow

--- src/main/java/com/muditasoft/springbootwebsocket/controller/UserController.java
+++ src/main/java/com/muditasoft/springbootwebsocket/controller/UserController.java
@@ -9,6 +9,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserController {
 
+    // @MessageMapping annotation ensures that if a message is sent to destination "/user"
+    // then the getUser() method is called
+    // The payload of the message is bound to a UserResponse object which is passed into getUser()
+    // After the 1 second delay, the getUser() method creates a UserResponse object and returns it.
+    // The return value is broadcast to all subscribers to "/topic/user" as specified in the @SendTo annotation.
+    // Note that the name form the input message is sanitized since in this case it will be echoed back and
+    // re-rendered in the browser dom on the client side.
     @MessageMapping("/user")
     @SendTo("/topic/user")
     public UserResponse getUser(User user) {
