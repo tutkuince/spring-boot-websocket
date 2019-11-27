9cdf866c6db7a8914ea3b5458b7fdd42207fcd53
tutkuince <tutku.ince@outlook.com>
2019-11-27 13:16:30 +0300

    SchedulerConfig created and SimpMessagingTemplate is injected

---
+++ src/main/java/com/muditasoft/springbootwebsocket/config/SchedulerConfig.java
@@ -0,0 +1,23 @@
+package com.muditasoft.springbootwebsocket.config;
+
+import com.muditasoft.springbootwebsocket.model.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling
+@Configuration
+public class SchedulerConfig {
+
+    // We create SimpMessagingTemplate for convert messages and send to broker
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @Scheduled(fixedDelay = 5000)
+    public void sendAdhocMessages() {
+        template.convertAndSend("/topic/user",
+                new UserResponse("Scheduler"));
+    }
+}
